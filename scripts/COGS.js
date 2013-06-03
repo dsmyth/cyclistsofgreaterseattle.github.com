ed6688045e5953159ddf6cf321a9a73c12692b44
COGS Webmaster <webmaster@cyclistsofgreaterseattle.org>
2013-06-02 22:53:29 -0700

    Fix IE7 problems with relative base href. Clean up COGS javascript.

--- scripts/COGS.js
+++ scripts/COGS.js
@@ -1,51 +1,69 @@
-<!--************** COGS Javascript Library ****************-->
+// ************** COGS Javascript Library
+/*jslint browser: true*/
+/*global $, jQuery*/
 
-var timeout	= 500;
-var closetimer	= 0;
-var ddmenuitem	= 0;
+var timeout   = 500;
+var closetimer   = 0;
+var ddmenuitem   = 0;
 
-// open hidden layer
-function mopen(id)
-{	
-	// cancel close timer
-	mcancelclosetime();
-
-	// close old layer
-	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
-
-	// get new layer and show it
-	ddmenuitem = document.getElementById(id);
-	ddmenuitem.style.visibility = 'visible';
-
-}
 // close showed layer
-function mclose()
-{
-	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+function mclose() {
+    "use strict";
+    if (ddmenuitem) {
+        ddmenuitem.style.visibility = 'hidden';
+    }
 }
 
 // go close timer
-function mclosetime()
-{
-	closetimer = window.setTimeout(mclose, timeout);
+function mclosetime() {
+    "use strict";
+    closetimer = window.setTimeout(mclose, timeout);
 }
 
 // cancel close timer
-function mcancelclosetime()
-{
-	if(closetimer)
-	{
-		window.clearTimeout(closetimer);
-		closetimer = null;
-	}
+function mcancelclosetime() {
+    "use strict";
+    if (closetimer) {
+        window.clearTimeout(closetimer);
+        closetimer = null;
+    }
+}
+
+// open hidden layer
+function mopen(id) {
+    "use strict";
+    // cancel close timer
+    mcancelclosetime();
+
+    // close old layer
+    if (ddmenuitem) {
+        ddmenuitem.style.visibility = 'hidden';
+    }
+
+    // get new layer and show it
+    ddmenuitem = document.getElementById(id);
+    ddmenuitem.style.visibility = 'visible';
 }
 
 // close layer when click-out
-document.onclick = mclose; 
+document.onclick = mclose;
 
 // functions to load included documents
-function loadCOGSHeader(){$("#COGSHeader").load("COGSContentPages/IncludeFiles/Include_COGSHeader.html");}
-function loadCOGSMenu(){$("#COGSMenu").load("COGSContentPages/IncludeFiles/Include_COGSMenu.html");}
-function loadCOGSLeft(){$("#COGSLeft").load("COGSContentPages/COGSLeftContent.html");}
-function loadCOGSFooter(){$("#COGSFooter").load("COGSContentPages/IncludeFiles/Include_COGSFooter.html");}
-<!--************** End of COGS Javascript Library ****************-->
+function loadCOGSHeader() {
+    "use strict";
+    $("#COGSHeader").load("COGSContentPages/IncludeFiles/Include_COGSHeader.html");
+}
+function loadCOGSMenu() {
+    "use strict";
+    $("#COGSMenu").load("COGSContentPages/IncludeFiles/Include_COGSMenu.html");
+}
+function loadCOGSLeft() {
+    "use strict";
+    $("#COGSLeft").load("COGSContentPages/COGSLeftContent.html");
+}
+function loadCOGSFooter() {
+    "use strict";
+    $("#COGSFooter").load("COGSContentPages/IncludeFiles/Include_COGSFooter.html");
+}
+// ************** End of COGS Javascript Library
+
